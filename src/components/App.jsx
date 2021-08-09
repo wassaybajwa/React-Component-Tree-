b31cc17529043c2790d3b6622254606c2bdfa47d
wassaybajwa <wassaybajwa@hotmail.com>
2021-08-08 21:34:14 -0400

    Component tree in React part-II

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,46 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-
-  const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
-
-  function handleChange(event){
-    
-    const newValue = event.target.value;
-    setInputText(newValue)
-  }
-
-  function addItem(){
-
-    setItems((preValue)=>{
-      return [...preValue, inputText]
-    })
-    setInputText("")
-  }
-
-  function deleteItem(id){
-
-    setItems(prevItem => {
-      return prevItem.filter(
-        (item, index) => {
-          return index !== id
-        }
-      )
-    })
-  }
   
+  const [items, setItems] = useState([]);
+  
+  
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea 
+        onAdd={addItem}
+      />
       <div>
         <ul>
-          {items.map((item, index) => (
-          <ToDoItem key={index} id={index} text={item} onChecked={deleteItem}/>
-        ))
-        }
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
         </ul>
       </div>
     </div>
